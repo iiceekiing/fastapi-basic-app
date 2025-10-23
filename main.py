83028943b4d1c001b1016ea94c762b129ba651a2
iiceekiing <iiceekiingsocials@gmail.com>
2025-10-23 18:49:48 +0100

    update: correct syntax error in main.py file

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ from typing import List
 
 app = FastAPI()
 
-db = List[User] = [
+db: List[User] = [
     User(id=uuid4(),
          first_name="iicee",
          last_name="kiing",
-         gender"male", 
-         role=[Role.admin, Role.user], age=25),
+         gender=Gender.male,
+         role=[Role.admin, Role.user],
+         age=25),
     
-        User(id=uuid4(),
+    User(id=uuid4(),
          first_name="dev",
          last_name="skript",
-         gender"male", 
-         role=[Role.user], age=20),
+         gender=Gender.male,
+         role=[Role.user], age=20)
 ]
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the User Management API"}
+
+@app.get("/api/v1/users")
+async def get_users():
+    return db
