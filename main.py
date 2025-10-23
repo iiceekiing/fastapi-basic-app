c4a8f65ca94eb31b0dba358511e0aa36d98aa89e
iiceekiing <iiceekiingsocials@gmail.com>
2025-10-23 19:03:13 +0100

    update: configure custom fixed user id generate using  "UUID"

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from models import User, Role, Gender
-from uuid import uuid4
+from uuid import UUID, uuid4
 from typing import List
 
-app = FastAPI()
+app = FastAPI(name="User Management API")
 
 db: List[User] = [
-    User(id=uuid4(),
+    User(id=UUID("c5bffb36-a7a9-48b5-b968-42c1d1204bec"),
          first_name="iicee",
          last_name="kiing",
          gender=Gender.male,
          role=[Role.admin, Role.user],
          age=25),
     
-    User(id=uuid4(),
+    User(id=UUID("76e6dd1f-6a6a-4f5f-81e8-26643b915ca6"),
          first_name="dev",
          last_name="skript",
          gender=Gender.male,
